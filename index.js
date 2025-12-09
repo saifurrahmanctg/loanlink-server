a47564824ef56a95533b500c38e59172bb7c8506
saifurrahmanctg <chysaifurrahmanbd@gmail.com>
2025-12-09 21:05:47 +0600

    Connected to MongoDB and addes env variable

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config({ path: ".env.local" });
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // Api's
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send(`LoanLink Server is running on port ${port} `);
