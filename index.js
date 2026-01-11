7af2f53705055ddd8a98ab73ca9d5310d8599b33
saifurrahmanctg <chysaifurrahmanbd@gmail.com>
2026-01-11 21:05:57 +0600

    chore: comment out redundant mongo connection calls for serverless optimization

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("loanlink-db");
     const loansCollection = db.collection("loans");
@@ -478,10 +478,10 @@ async function run() {
       }
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // await client.close();
   }
